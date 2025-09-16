963a80d61f7813a78cfb33bd44c3ee61fc95a161
Felix Kebaki <felixkebaki@gmail.com>
2025-09-16 12:24:48 +0300

    serving frontend to express

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const cors=require("cors");
 const cookieParser = require("cookie-parser");
 const dotenv=require("dotenv").config()
 const connectDb=require("./config/connectDb")
+const path = require("path");
 
 const app=express()
 const PORT=process.env.PORT || 5000;
@@ -35,6 +36,16 @@ app.use("/api/invitations",require("./routers/inviteRouter"))
 app.use("/api/upcomings",require("./routers/upcomingRouter"))
 app.use("/api/allCategory",require("./routers/getAllCategoryRouter"))
 app.use("/api/allLength",require("./routers/dashboardRouter"))
+
+
+// Serve frontend
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "frontend/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend/build", "index.html"));
+  });
+}
 
 app.listen(PORT,()=>{
     console.log(`Server listening to port ${PORT}...`)
