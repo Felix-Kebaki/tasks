8dd09641f49a333df57da719bd470a1d2e1a5bb8
Felix Kebaki <felixkebaki@gmail.com>
2025-09-16 12:52:47 +0300

    fixing the directory

--- backend/index.js
+++ backend/index.js
@@ -40,12 +40,13 @@ app.use("/api/allLength",require("./routers/dashboardRouter"))
 
 // Serve frontend
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "frontend/build")));
+  app.use(express.static(path.join(__dirname, "..", "frontend", "build")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "..", "frontend", "build", "index.html"));
   });
 }
+
 
 
 app.listen(PORT,()=>{
