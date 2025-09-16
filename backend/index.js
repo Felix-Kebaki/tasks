b362411a23b108d1a57c20a5e7216b2738be4fd7
Felix Kebaki <felixkebaki@gmail.com>
2025-09-16 12:43:44 +0300

    Second attempt of solving

--- backend/index.js
+++ backend/index.js
@@ -40,12 +40,13 @@ app.use("/api/allLength",require("./routers/dashboardRouter"))
 
 // Serve frontend
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "frontend")));
+  app.use(express.static(path.join(__dirname, "frontend/build")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "frontend", "index.html"));
+    res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
   });
 }
+
 
 app.listen(PORT,()=>{
     console.log(`Server listening to port ${PORT}...`)
