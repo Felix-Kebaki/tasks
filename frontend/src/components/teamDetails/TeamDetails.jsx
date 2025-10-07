a6bb1643cbbe79d1f314a4512a99f599ecd1fcae
Felix Kebaki <felixkebaki@gmail.com>
2025-10-07 18:56:41 +0300

    Team dashboard 1

--- frontend/src/components/teamDetails/TeamDetails.jsx
+++ frontend/src/components/teamDetails/TeamDetails.jsx
@@ -7,6 +7,10 @@ import { Loading } from "../loading/Loading";
 
 import { useGetTeamdashboardQuery } from "../../redux/api/teamApiSlice";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFolder } from "@fortawesome/free-solid-svg-icons";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+
 export function TeamDetails() {
   const param = useParams();
 
@@ -16,8 +20,11 @@ export function TeamDetails() {
     isLoading,
   } = useGetTeamdashboardQuery({ id: param.id });
 
+  const HandleCreateTeamtask = () => {};
+
   useEffect(() => {
     refetch();
+    console.log(teamDashboard);
   }, [refetch]);
 
   if (isLoading) {
@@ -33,15 +40,83 @@ export function TeamDetails() {
       <div className="TeamDetailsMainDiv">
         <div className="TeamDetailTopDiv">
           <p className="TeamName title">{teamDashboard?.name}</p>
-          <p className="DateOfTeamCreation text">Created on {moment(teamDashboard?.createdAt).format("YYYY Do MMMM")}</p>
+          <p className="DateOfTeamCreation text">
+            Created on {moment(teamDashboard?.createdAt).format("Do MMMM YYYY")}
+          </p>
         </div>
 
-    {
-
-    }
+        <div className="TeamtaskAndGraphMainWrapper">
+          {teamDashboard?.teamtask.length === 0 && !teamDashboard?.isAdmin ? (
+            <div className="NoTeamtaskMainDiv">
+              <p className="text">
+                The admin hasn't created any Teamtask yet. You will be notified
+                when that happens.
+              </p>
+            </div>
+          ) : teamDashboard?.teamtask.length === 0 && teamDashboard?.isAdmin ? (
+            <div className="NoTeamtaskAdminMainDiv">
+              <div className="TeamtaskMainTitleDiv">
+                <p className="TeamtaskMainTitle title">Teamtasks</p>
+                <p className="TeamTaskLengthMain text">
+                  {teamDashboard?.teamtask.length}
+                </p>
+              </div>
+              <div className="TeamtaskCreateOneAndFewInfoMainDiv">
+                <div className="CreateTeamtaskMainDiv">
+                  <FontAwesomeIcon
+                    icon={faFolder}
+                    className="CreateTeamtaskFolderIcon"
+                  />
+                  <p className="TeamtaskCreateText text">
+                    <FontAwesomeIcon
+                      icon={faPlus}
+                      className="CreateTeamtaskplusIcon"
+                    />
+                    Create Teamtask.
+                  </p>
+                </div>
+                <div className="FewInfoOnCreatingTeamtaskDiv">
+                  <p className="title">About Team Tasks</p>
+                  <div className="text">
+                    <p>
+                      1. Create and manage tasks for your team in one place.
+                    </p>
+                    <p>
+                      2. Assign specific members to handle individual
+                      responsibilities.
+                    </p>
+                    <p>3. Track each member’s task progress and submissions.</p>
+                    <p>4. Monitor overall task completion and performance.</p>
+                    <p>
+                      5. Manage multiple team tasks under the same team for
+                      better organization.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ) : (
+            <div className="TeamTaskAndGraphMainDiv">
+              <div className="TeamtaskMainDivAtTeamDetails">
+                <div className="TeamtaskMainTitleDiv">
+                  <p className="TeamtaskMainTitle title">Teamtasks</p>
+                  <p className="TeamTaskLengthMain text">
+                    {teamDashboard?.teamtask.length}
+                  </p>
+                </div>
+              </div>
+              <div className="GraphOnTeamDashboardMainDiv"></div>
+            </div>
+          )}
+        </div>
 
         <div className="MembersTableMainDivWrapper">
-          <p className="MembersTableMainTitle title">Members</p>
+          <div className="MembersTableMainTitleDiv title">
+            <p className="MembersTableMainTitle">Members</p>
+            <p className="MembersTableMainLength text">
+              {teamDashboard?.members.length}
+            </p>
+          </div>
           <table border="1" cellSpacing="0" className="MembersTableMainDiv">
             <thead>
               <tr className="MemberTableHeadingRow text">
@@ -61,12 +136,26 @@ export function TeamDetails() {
                     {member.firstName} {member.lastName}
                   </td>
                   <td>{member.email}</td>
-                  <td className={member.email===teamDashboard.admin[2]?"AdminTextColor":"NotAdminTextColor"}>{member.email === teamDashboard.admin[2]?"Yes":"No"}</td>
+                  <td
+                    className={
+                      member.email === teamDashboard.admin[2]
+                        ? "AdminTextColor"
+                        : "NotAdminTextColor"
+                    }
+                  >
+                    {member.email === teamDashboard.admin[2] ? "Yes" : "No"}
+                  </td>
                   <td>{member.assignedTasks}</td>
                   <td>{member.completeTasks}</td>
                   <td>{member.submissions}</td>
                   {teamDashboard.isAdmin ? (
-                    <td className={member.active!==undefined?"ActiveOnLastLogged":"NotActiveOnLastLogged"}>
+                    <td
+                      className={
+                        member.active !== undefined
+                          ? "ActiveOnLastLogged"
+                          : "NotActiveOnLastLogged"
+                      }
+                    >
                       {member.active !== undefined
                         ? member.active
                         : moment(member.loggedOut).isSame(moment(), "day")
